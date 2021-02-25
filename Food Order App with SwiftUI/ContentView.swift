40162d1154ea5fb4dc9579fa092695a88cb34200
@ranaaits19 <ranaaits19@gmail.com>
2021-02-25 22:31:59 +0600

    Initial Commit

---
+++ Food Order App with SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food Order App with SwiftUI
+//
+//  Created by Solayman Rana on 25/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food Order App with SwiftUI/Food_Order_App_with_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Order_App_with_SwiftUIApp.swift
+//  Food Order App with SwiftUI
+//
+//  Created by Solayman Rana on 25/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_Order_App_with_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
